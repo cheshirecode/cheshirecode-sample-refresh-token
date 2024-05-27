8054a513cdbe98385b9a6036ddda9a763b955388
cheshireCode <1631630+cheshirecode@users.noreply.github.com>
2024-05-27 16:19:51 +0000

    proper linting config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,27 @@
+require("@rushstack/eslint-patch/modern-module-resolution");
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const baseConfig = require("@fieryeagle/eslint-config-react");
+
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  extends: ["@fieryeagle/eslint-config-react"],
+  parserOptions: {
+    ...baseConfig.parserOptions,
+    tsconfigRootDir: __dirname,
+  },
+  settings: {
+    ...baseConfig.settings,
+    "import/resolver": {
+      typescript: {},
+    },
+    "import/core-modules": ["virtual:uno.css"],
+  },
+  plugins: [...baseConfig.plugins, "react-refresh"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    ...baseConfig.rules,
+    "@typescript-eslint/ban-ts-comment": "off",
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
   },
-}
+};
