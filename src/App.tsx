7e5d9dc45b5adb02d8428ade296f1a316f001b80
cheshireCode <1631630+cheshirecode@users.noreply.github.com>
2024-05-27 16:20:58 +0000

    initial page

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,37 @@
-import { useState } from 'react';
-import main from '../lib/main';
-import './App.css';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-console.log(main)
+import cx from "classnames";
+
+import loginImg from "./assets/login.svg"; // https://undraw.co/illustrations
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex h-screen w-full">
+      <div className="hidden lg:flex items-center justify-center flex-1 bg-white text-black">
+        <div className="max-w-md text-center">
+          <img src={loginImg} alt="login" height={490} width={640} />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="w-full bg-gray-100 lg:w-1/2 flex items-center justify-center">
+        <div className="max-w-md w-full p-6">
+          <h1 className="text-3xl font-semibold mb-6 text-black text-center">
+            Unauth
+          </h1>
+          <div className="mt-4 text-sm text-gray-600 text-center">
+            <button
+              type="button"
+              className={cx(
+                "text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg",
+                "text-sm px-5 py-2.5 me-2 mb-2",
+                "dark:bg-blue-600 dark:hover:bg-blue-700 border-0",
+                "cursor-pointer",
+              )}
+            >
+              Login
+            </button>
+          </div>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import 'uno.css'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
+/* eslint-disable sort-imports */
+import "./styles/index.css";
+import "./styles/reset.css";
+/* eslint-enable sort-imports */
+import "virtual:uno.css";
+// uno
+import App from "./App.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </StrictMode>,
+);
