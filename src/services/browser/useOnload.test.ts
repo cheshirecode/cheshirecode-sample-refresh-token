4b63a23d34e2b7d806b749385f9b407c26c8900b
cheshireCode <1631630+cheshirecode@users.noreply.github.com>
2024-06-06 15:30:42 +0000

    add folder aliases

--- src/services/browser/useOnload.test.ts
+++ src/services/browser/useOnload.test.ts
@@ -3,7 +3,7 @@
  */
 
 import { renderHook, waitFor } from "@testing-library/react";
-import { describe, expect, it, vi } from "vitest";
+import { describe, expect, it } from "vitest";
 
 import useOnLoad, { useAuthParams } from "./useOnLoad";
 
